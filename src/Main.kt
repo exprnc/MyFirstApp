837bf7a27461412164035175ee4d2db4a9e98371
Alfir Rakhimov <exprnc7@gmail.com>
2023-12-22 20:58:35 +0500

    updated my first app

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val x = 3
+    val y = 1
+    println(if (x > y) "x is greater than y" else "x is not greater than y")
+    println("This line runs no matter what")
+}
